1742b2449ebd66adea1c189726cc31414264c6b5
benjithompson <mail@benjithompson.com>
2019-06-29 22:06:47 -0500

    added surrogateid, options, split execution pattern function

--- ToscaCIConfig/helpers.cs
+++ ToscaCIConfig/helpers.cs
@@ -6,21 +6,61 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Xml;
 
 namespace ToscaCIConfig
 {
     static class Helpers
     {
-        public static bool MatchesExecutionPattern(string execution)
+
+        public static TestConfig GetTestConfig(string mode, string configName)
         {
-            if(Regex.IsMatch(execution, "^(/.*).*$") || Regex.IsMatch(execution, "[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}"))
+            ObservableCollection<TestConfig> testConfigs = GetTestConfigsCollection(mode);
+            try
             {
-                return true;
-            }else
+                var match = testConfigs.First(p => (p.Mode == mode && p.Name == configName));
+                return match;
+            }
+            catch (Exception e)
             {
-                return false;
-            } 
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+        public static ObservableCollection<TestConfig> GetTestConfigsCollection(string mode)
+        {
+            try { 
+            
+                ObservableCollection<TestConfig> configs;
+
+                switch (mode)
+                {
+                    case "DEX":
+                        configs = ((MainWindow)Application.Current.MainWindow)?.DexConfigsCollection;
+                        break;
+                    case "Remote":
+                        configs = ((MainWindow)Application.Current.MainWindow)?.RemoteConfigsCollection;
+                        break;
+                    case "Local":
+                        configs = ((MainWindow)Application.Current.MainWindow)?.LocalConfigsCollection;
+                        break;
+                    default:
+                        Console.WriteLine(@"No compatible Execution Mode for implemented exceptions");
+                        configs = null;
+                        break;
+                }
+
+                return configs;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
         }
 
         public static string RemoveExecutionMode(string filename, string exType)
@@ -41,16 +81,16 @@ namespace ToscaCIConfig
         }
 
         //Todo: SurrogateIDs, ignoreNonMatchingIds, buildrootfolder, cleanoldresults, testmandatename
-        public static XmlNodeList getExecutionsListFromTestConfigFile(string configDir, string configname, string executionmode)
+        public static XmlNodeList getExecutionsNodeListFromTestConfigFile(string dir, string name, string mode)
         {
 
-            var configpath = configDir + executionmode + "_" + configname + ".xml";
+            var configpath = dir + mode + "_" + name + ".xml";
             XmlNodeList exList = null;
             XmlDocument configDoc = new XmlDocument();
             try
             {
                 configDoc.Load(configpath);
-                if (executionmode == "DEX")
+                if (mode == "DEX")
                 {
                     exList = configDoc.GetElementsByTagName("TestEvent");
                     for (int i = 0; i < exList.Count; i++)
@@ -76,10 +116,43 @@ namespace ToscaCIConfig
             return exList;
         }
 
-        public static XmlNodeList getPropertiesListFromTestConfigFile(string configDir, string configname, string executionmode)
+        public static void SetTestConfigOptionsFromNodeList(TestConfig config)
         {
 
-            var configpath = configDir + executionmode + "_" + configname + ".xml";
+        }
+        public static XmlNodeList getSurrogateIdsNodeListFromTestConfigFile(string dir, string name, string mode, string tag)
+        {
+
+            var configpath = dir + mode + "_" + name + ".xml";
+            XmlNodeList surrogateList = null;
+            XmlDocument configDoc = new XmlDocument();
+            try
+            {
+                configDoc.Load(configpath);
+                if (mode != "DEX")
+                {
+                    surrogateList = configDoc.GetElementsByTagName(tag);
+                    for (int i = 0; i < surrogateList.Count; i++)
+                    {
+                        Console.WriteLine(surrogateList[i].InnerText);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return surrogateList;
+
+        }
+        
+
+        public static XmlNodeList getPropertiesNodeListFromTestConfigFile(string dir, string name, string mode)
+        {
+
+            var configpath = dir + mode + "_" + name + ".xml";
             XmlDocument configDoc = new XmlDocument();
             try
             {
@@ -99,7 +172,38 @@ namespace ToscaCIConfig
             }
         }
 
-        public static ObservableCollection<Execution> getExecutionCollectionFromNodeList(XmlNodeList nodelist)
+        public static void setTestConfigOptionsFromFile(TestConfig config)
+        {
+
+            string dir = config.Path;
+            string mode = config.Mode;
+
+            XmlNodeList modeNode = null;
+            XmlDocument xml = new XmlDocument();
+            try
+            {
+                xml.Load(dir);
+                if (mode != "DEX")
+                {
+                    modeNode = xml.GetElementsByTagName("ignoreNonMatchingIds");
+                    config.ignoreNonMatchingSurrogateIds = modeNode[0].InnerText.ToLower() == "false";
+                    modeNode = xml.GetElementsByTagName("cleanoldresults");
+                    config.CleanOldResults = modeNode[0].InnerText.ToLower() == "false";
+                    modeNode = xml.GetElementsByTagName("buildrootfolder");
+                    config.BuildRootFolder = modeNode[0].InnerText;
+                    modeNode = xml.GetElementsByTagName("testMandateName");
+                    config.TestMandateName = modeNode[0].InnerText;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public static ObservableCollection<Execution> GetExecutionCollectionFromNodeList(XmlNodeList nodelist)
         {
             var c = new ObservableCollection<Execution>();
             for (int i = 0; i < nodelist.Count; i++)
@@ -112,7 +216,20 @@ namespace ToscaCIConfig
             return c;
         }
 
-        public static ObservableCollection<CustomProperty> getPropertyCollectionFromNodeList(XmlNodeList nodelist)
+        public static ObservableCollection<SurrogateId> GetSurrogateIdsCollectionFromNodeList(XmlNodeList nodelist)
+        {
+            var c = new ObservableCollection<SurrogateId>();
+            for (int i = 0; i < nodelist.Count; i++)
+            {
+                if (nodelist[i].InnerText != "")
+                {
+                    c.Add(new SurrogateId(nodelist[i].InnerText));
+                }
+            }
+            return c;
+        }
+
+        public static ObservableCollection<CustomProperty> GetPropertyCollectionFromNodeList(XmlNodeList nodelist)
         {
             var c = new ObservableCollection<CustomProperty>();
             for (int i = 0; i < nodelist.Count; i++)
@@ -146,6 +263,20 @@ namespace ToscaCIConfig
         private static void writeListViewsToTestConfigFile(string propertyName, string propertyValue)
         {
             Console.WriteLine("todo: write propertyname and value to xml");
+        }
+        public static bool ExecutionPatternIsValid(string ex)
+        {
+            return (IsNodePath(ex) || IsSurrogateId(ex));
+        }
+
+        public static bool IsNodePath(string exStr)
+        {
+            return Regex.IsMatch(exStr, "^(/.*).*$");
+        }
+
+        public static bool IsSurrogateId(string exStr)
+        {
+            return Regex.IsMatch(exStr, "[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}");
         }
     }
 }
