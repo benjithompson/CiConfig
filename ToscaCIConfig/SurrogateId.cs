79cc5022b997f058651afec2a9e5449b3e7faab3
benjithompson <mail@benjithompson.com>
2019-06-29 22:10:37 -0500

    preliminary surrogateid handling

---
+++ ToscaCIConfig/SurrogateId.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToscaCIConfig
+{
+    internal class SurrogateId
+    {
+        public SurrogateId(string id)
+        {
+            this.Surrogateid = id;
+
+        }
+        public string Surrogateid { get; set; }
+    }
+}
+
