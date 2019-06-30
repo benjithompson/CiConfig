6f736e4d2f476a8442e300da4b7190b721d83b7b
benjithompson <mail@benjithompson.com>
2019-06-29 22:10:58 -0500

    added options to testconfig objects

--- ToscaCIConfig/TestConfig.cs
+++ ToscaCIConfig/TestConfig.cs
@@ -10,12 +10,21 @@ namespace ToscaCIConfig
     {
         public TestConfig(string type, string name, string path)
         {
-            ConfigType = type;
-            ConfigName = name;
-            ConfigPath = path;
+            Mode = type;
+            Name = name;
+            Path = path;
+            ignoreNonMatchingSurrogateIds = true;
+            CleanOldResults = true;
+
         }
-        public string ConfigType { get; set; }
-        public string ConfigName { get; set; }
-        public string ConfigPath { get; set; }
+
+        public bool ignoreNonMatchingSurrogateIds { get; set; }
+        public bool CleanOldResults { get; set; }
+        public string Mode { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string BuildRootFolder { get; set; }
+        public string TestMandateName { get; set; }
+
     }
 }
